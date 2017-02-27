529be336a613e5882f8826e5b6fde7b863e661d5
OLK Macbook Pro <OLK@macbook-pro-ol.local>
2017-02-27 22:11:21 +0200

    init commit

---
+++ CookBook 1952/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CookBook 1952
+//
+//  Created by OLK Macbook Pro on 23.02.17.
+//  Copyright © 2017 Oleg Kalinkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    } 
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
