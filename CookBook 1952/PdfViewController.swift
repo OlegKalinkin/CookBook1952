0308deed965c8065a85efb0b6d6df692dbf632d4
OLK Macbook Pro <OLK@macbook-pro-ol.local>
2017-03-08 22:05:59 +0200

    Download local PDF

--- CookBook 1952/PdfViewController.swift
+++ CookBook 1952/PdfViewController.swift
@@ -13,13 +13,44 @@ class PdfViewController: UIViewController {
     //Переменные куда будем передавать информацию из PdfListController
     var localPdfUrls: URL?
     var remotePdfUrls: URL?
+    var pdfDocument: CGPDFDocument? /*Когда скачивается pdf из интернета создается документ с информацией содержащейся в нем*/
     
-
+    @IBOutlet weak var progressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        //Делаем проверку на предмет какой метод вызывать
+        if localPdfUrls != nil {
+            self.loadLocalPdf()
+        }
+        else if remotePdfUrls != nil {
+            self.loadRemotePdf()
+        }
+        
     }
-
-   
+    
+    // Реализуем функцию загрузки локальных PDF:
+    func loadLocalPdf() {
+        progressView.isHidden = true //Скрываем ProgressView для локального файла
+        
+        let pdfAsData = NSData(contentsOf: localPdfUrls!) //Представляем PDF как данные
+        
+        let dataProvider = CGDataProvider(data: pdfAsData!) /*Пропускае данные через CGDataProvider, что бы не беспокоиться о буфере данных*/
+        
+        self.pdfDocument = CGPDFDocument(dataProvider!) /* CGPDFDocument и CGDataProvider это классы фреймворка CoreGraphik */
+        
+        self.navigationItem.title = localPdfUrls?.deletingPathExtension().lastPathComponent // Отображаем название документа в NavigationController
+        
+        self.preparePageViewController() //Метод отвечающий за отображение PDF документа
+    }
+    
+    func loadRemotePdf() {
+        
+    }
+    
+    func preparePageViewController() {
+        
+    }
 }
