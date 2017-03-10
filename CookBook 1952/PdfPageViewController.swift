7cac7580a2baaced820ee98da497144906fd696a
OLK Macbook Pro <OLK@macbook-pro-ol.local>
2017-03-10 14:19:23 +0200

    Created full class PdfPageView

--- CookBook 1952/PdfPageViewController.swift
+++ CookBook 1952/PdfPageViewController.swift
@@ -13,6 +13,41 @@ class PdfPageView: UIView {
     var pdfDocument: CGPDFDocument? = nil
     var pageNumber: Int? = nil
     
+    // Для работы с CoreGraphik вызываем метод draw rect:
+    override func draw(_ rect: CGRect) {
+        if pdfDocument != nil {
+            let ctx = UIGraphicsGetCurrentContext()
+            
+            UIColor.white.set()
+            
+            ctx?.fill(rect)
+            
+            _ = ctx?.ctm // вызываем CGAffineTransform
+            _ = ctx?.scaleBy(x: 1, y: -1)
+            _ = ctx?.translateBy(x: 0, y: -rect.size.height)
+            
+            let page = pdfDocument?.page(at: pageNumber!) // Объявляем страницу
+            // Далее производим настройки отображения страницы:
+            let pageRect = page?.getBoxRect(CGPDFBox.cropBox)
+            
+            let ratioWidth = rect.size.width / (pageRect?.size.width)!
+            let ratioHeight = rect.size.height / (pageRect?.size.height)!
+            
+            let ratio = min(ratioWidth, ratioHeight)
+            
+            let newWidth = (pageRect?.size.width)! * ratio
+            let newHeight = (pageRect?.size.height)! * ratio
+            
+            let ofsetX = (rect.size.width - newWidth)
+            let ofsetY = (rect.size.height - newHeight)
+            
+            ctx?.scaleBy(x: -(pageRect?.origin.x)! + ofsetX, y: -(pageRect?.origin.y)! + ofsetY)
+            
+            ctx?.drawPDFPage(page!)
+        }
+        
+    }
+    
 }
 
 class PdfPageViewController: UIViewController, UIScrollViewDelegate {
@@ -21,10 +56,10 @@ class PdfPageViewController: UIViewController, UIScrollViewDelegate {
     var pageNumber: Int? = nil
     
     @IBOutlet weak var page: PdfPageView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         self.preparePdfPage()
         
